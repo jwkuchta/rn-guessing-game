db33806acc66c740a6dabdaad18d024f269f0d79
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-24 18:57:17 -0400

    added styling to the Header component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>I am a screen</Text>
+            <Text style={styles.title}>Start a New Game!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number: </Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => {}}/>
+                    <Button title="Confirm" onPress={() => {}}/>
+                </View>
+            </View>
         </View>
     )
 }
@@ -14,6 +22,22 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10, // replaces marginBottom + marginTop
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center', // left to right
+
+    },
+    buttonContainer: {
+        flexDirection: 'row', //so the items are next to each other
+        width: '100%', // to use the width of the parent and not just as much as the buttons need
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 })
 
