b9b5ee1d96056eb2cf1f6686ef744a0f3345073d
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-24 19:49:41 -0400

    configured keyboard toggling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,25 +1,53 @@
-import React from 'react'
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Button, 
+    TouchableWithoutFeedback,
+    Keyboard // this is not a component it's an api
+} from 'react-native'
 import Card from '../components/Card'
 import colors from '../constants/colors'
+import Input from '../components/Input'
 
 const StartGameScreen = props => {
+
+    const [ enteredValue, setEnteredValue ] = useState()
+
+    const numberInputHandler = (inputText) => {
+        // replace anything that's not a number with an empty string
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    }
+
+    // we have to wrap everything in TouchableWithoutFeedback component so the keyboard is
+    // toggled away when the user clicks outside of the input area
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number: </Text>
-                <TextInput />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title="Reset" color={colors.accent} onPress={() => {}}/>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number: </Text>
+                    <Input 
+                    style={styles.input} 
+                    blurOnSubmit 
+                    autoCapitalize='none' 
+                    autoCorrect={false}
+                    keyboardType='number-pad'
+                    maxLength={2}
+                    value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" color={colors.accent} onPress={() => {}}/>
+                        </View>
+                        <View style={styles.button}>
+                            <Button title="Confirm" color={colors.primary} onPress={() => {}}/>
+                        </View>
                     </View>
-                    <View style={styles.button}>
-                        <Button title="Confirm" color={colors.primary} onPress={() => {}}/>
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -46,6 +74,10 @@ const styles = StyleSheet.create({
     },
     button: {
         width: '40%'
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
     }
 })
 
